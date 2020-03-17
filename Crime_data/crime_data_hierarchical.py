f920f31fc0a2f5e503f0fd8e18c60f79715d4f73
shantanudn <shantanu.dn@gmail.com>
2020-03-17 17:48:45 +0530

    Add files via upload

--- Crime_data/crime_data_hierarchical.py
+++ Crime_data/crime_data_hierarchical.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pylab as plt 
-Crime = pd.read_csv("C:/Training/Analytics/Clustering/crime_data.csv")
+Crime = pd.read_csv("C:/Training/Analytics/Clustering/Crime_data/crime_data.csv")
 
 # Normalization function 
 #def norm_func(i):
@@ -22,9 +22,11 @@ import scipy.cluster.hierarchy as sch # for creating dendrogram
 
 type(df_norm)
 
-#p = np.array(df_norm) # converting into numpy array format 
+# =============================================================================
+# With method single Linkage and euclidean metric
+# =============================================================================
 
-z = linkage(df_norm, method="complete",metric="euclidean")
+z = linkage(df_norm, method="single",metric="euclidean")
 
 plt.figure(figsize=(15, 5));plt.title('Hierarchical Clustering Dendrogram');plt.xlabel('Index');plt.ylabel('Distance')
 sch.dendrogram(
@@ -38,17 +40,140 @@ help(linkage)
 
 # Now applying AgglomerativeClustering choosing 3 as clusters from the dendrogram
 from	sklearn.cluster	import	AgglomerativeClustering 
+h_single	=	AgglomerativeClustering(n_clusters=3,	linkage='single',affinity = "euclidean").fit(df_norm) 
+
+
+cluster_labels=pd.Series(h_single.labels_)
+
+Crime_single = Crime
+Crime_single['clust']=cluster_labels # creating a  new column and assigning it to new column 
+Crime_single = Crime.iloc[:,[5,0,1,2,3,4]]
+Crime_single.head()
+
+# getting aggregate mean of each cluster
+Crime_single.iloc[:,2:].groupby(Crime_single.clust).median()
+
+# =============================================================================
+# With method complete linkage and euclidean metric
+# =============================================================================
+
+#p = np.array(df_norm) # converting into numpy array format 
+
+z = linkage(df_norm, method="complete",metric="euclidean")
+
+plt.figure(figsize=(15, 5));plt.title('Hierarchical Clustering Dendrogram');plt.xlabel('Index');plt.ylabel('Distance')
+sch.dendrogram(
+    z,
+    leaf_rotation=0.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
+
+# Now applying AgglomerativeClustering choosing 3 as clusters from the dendrogram
+from	sklearn.cluster	import	AgglomerativeClustering 
 h_complete	=	AgglomerativeClustering(n_clusters=4,	linkage='complete',affinity = "euclidean").fit(df_norm) 
 
 
 cluster_labels=pd.Series(h_complete.labels_)
-
-Crime['clust']=cluster_labels # creating a  new column and assigning it to new column 
-Crime = Crime.iloc[:,[5,0,1,2,3,4]]
-Crime.head()
+Crime_complete = Crime
+Crime_complete['clust']=cluster_labels # creating a  new column and assigning it to new column 
+Crime_complete = Crime_complete.iloc[:,[5,0,1,2,3,4]]
+Crime_complete.head()
 
 # getting aggregate mean of each cluster
-Crime.iloc[:,2:].groupby(Crime.clust).median()
+Crime_complete.iloc[:,2:].groupby(Crime_complete.clust).median()
 
-# creating a csv file 
-Crime.to_csv("Crime_grouped.csv",encoding="utf-8")
+# =============================================================================
+# With method average linkage and euclidean metric
+# =============================================================================
+
+#p = np.array(df_norm) # converting into numpy array format 
+
+z = linkage(df_norm, method="average",metric="euclidean")
+
+plt.figure(figsize=(15, 5));plt.title('Hierarchical Clustering Dendrogram');plt.xlabel('Index');plt.ylabel('Distance')
+sch.dendrogram(
+    z,
+    leaf_rotation=0.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
+
+# Now applying AgglomerativeClustering choosing 3 as clusters from the dendrogram
+from	sklearn.cluster	import	AgglomerativeClustering 
+h_complete	=	AgglomerativeClustering(n_clusters=5,	linkage='average',affinity = "euclidean").fit(df_norm) 
+
+
+cluster_labels=pd.Series(h_complete.labels_)
+Crime_average = Crime
+Crime_average['clust']=cluster_labels # creating a  new column and assigning it to new column 
+Crime_average = Crime_average.iloc[:,[5,0,1,2,3,4]]
+Crime_average.head()
+
+# getting aggregate mean of each cluster
+Crime_average.iloc[:,2:].groupby(Crime_average.clust).median()
+
+# =============================================================================
+# With method ward linkage and euclidean metric
+# =============================================================================
+
+#p = np.array(df_norm) # converting into numpy array format 
+
+z = linkage(df_norm, method="ward",metric="euclidean")
+
+plt.figure(figsize=(15, 5));plt.title('Hierarchical Clustering Dendrogram');plt.xlabel('Index');plt.ylabel('Distance')
+sch.dendrogram(
+    z,
+    leaf_rotation=0.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
+
+# Now applying AgglomerativeClustering choosing 3 as clusters from the dendrogram
+from	sklearn.cluster	import	AgglomerativeClustering 
+h_complete	=	AgglomerativeClustering(n_clusters=2,	linkage='ward',affinity = "euclidean").fit(df_norm) 
+
+
+cluster_labels=pd.Series(h_complete.labels_)
+Crime_ward = Crime
+Crime_ward['clust']=cluster_labels # creating a  new column and assigning it to new column 
+Crime_ward = Crime_ward.iloc[:,[5,0,1,2,3,4]]
+Crime_ward.head()
+
+# getting aggregate mean of each cluster
+Crime_ward.iloc[:,2:].groupby(Crime_ward.clust).median()
+
+
+# =============================================================================
+# Distance
+# =============================================================================
+
+# =============================================================================
+# With method complete linkage and Manhattan metric
+# =============================================================================
+
+#p = np.array(df_norm) # converting into numpy array format 
+
+z = linkage(df_norm, method="complete",metric="cityblock")
+
+plt.figure(figsize=(15, 5));plt.title('Hierarchical Clustering Dendrogram');plt.xlabel('Index');plt.ylabel('Distance')
+sch.dendrogram(
+    z,
+    leaf_rotation=0.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
+
+# Now applying AgglomerativeClustering choosing 3 as clusters from the dendrogram
+from	sklearn.cluster	import	AgglomerativeClustering 
+h_complete	=	AgglomerativeClustering(n_clusters=2,	linkage='complete',affinity = "cityblock").fit(df_norm) 
+
+
+cluster_labels=pd.Series(h_complete.labels_)
+Crime_complete = Crime
+Crime_complete['clust']=cluster_labels # creating a  new column and assigning it to new column 
+Crime_complete = Crime_complete.iloc[:,[5,0,1,2,3,4]]
+Crime_complete.head()
+
+# getting aggregate mean of each cluster
+Crime_complete.iloc[:,2:].groupby(Crime_complete.clust).median()
